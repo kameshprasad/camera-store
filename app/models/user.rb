519f7a2c745984e3a18e6a17f512b77d603ffaf2
Kamesh Prasad <kamesh@localdev>
2020-01-16 22:46:56 +0530

    Added Doorkeeper for securing API

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,14 @@ class User < ApplicationRecord
             presence: true
 
   validates :email, uniqueness: { case_sensitive: false }
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
 end
